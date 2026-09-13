5d0bbb8f27c597f3e8aa1e9cd400e8ca429a442f
Tihon Petrashov <tpetrasov2@gmail.com>
2026-09-13 19:57:42 +0300

    8 save

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 from calculator_modules.arithmetic import add, subtract, multiply, divide;
 from advanced import power, square_root;
 
-try:
-    x=int(input())
-    y=int(input())
+class NegativeNumberError(Exception):
+            """исключение если отрицательное число"""
+            pass
 
-    result=0
-    a = input("введите операцию (сложение,вычитание,умножение,деление,стпень)").lower().strip()
-    if a=="сложение":
-        result=add(x,y)
-        print(result)
+def main():
+    try:
+        x=int(input())
+        y=int(input())
 
-    if a=="вычитание":
-        result=subtract(x,y)
-        print(result)
+        result=0
+        a = input("введите операцию (сложение,вычитание,умножение,деление,стпень,корень-только первое число,второе-любое)").lower().strip()
+        if a=="сложение":
+            result=add(x,y)
+            print(result)
 
-    if a=="умножение":
-        result=multiply(x,y)
-        print(result)
+        if a=="вычитание":
+            result=subtract(x,y)
+            print(result)
 
-
-    if a=="деление":
-        result=divide(x,y)
-        print(result)
-
-    if a=="степень":
-        result=power(x,y)
-        print(result)
+        if a=="умножение":
+            result=multiply(x,y)
+            print(result)
 
 
-    # if __name__ == "__main__":
-    #     main()
-# except ZeroDivisionError:
-#     print("нельзя делить на ноль")
+        if a=="деление":
+            result=divide(x,y)
+            print(result)
 
-except ValueError:
-    print("введите правильный тип данных")
+        if a=="степень":
+            result=power(x,y)
+            print(result)
 
-finally:
-    print("завершено")
+
+        def sqrt_with_check(x):
+            """корень с проверкой.Для отрицательного числа выбрасывает свое исключение"""
+            if x<0:
+                raise NegativeNumberError("ошибка:корень из отрицательного числа не определен")
+            return x**0.5
+
+        if a=="корень":
+            result=sqrt_with_check(x)
+            print(result)
+
+
+        # if __name__ == "__main__":
+        #     main()
+    # except ZeroDivisionError:
+    #     print("нельзя делить на ноль")
+
+
+
+
+
+    except ValueError:
+        print("введите правильный тип данных")
+
+
+    except NegativeNumberError as e:
+        print(e)
+
+    finally:
+        print("завершено")
